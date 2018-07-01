71de0a0f12f43114e7e52bfccf43870eb276943e
MelroyNoronha <melmax1208@gmail.com>
2018-07-01 19:43:19 +0530

    libgdx project setup

---
+++ desktop/src/com/libgdx/tetris_clone/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.libgdx.tetris_clone.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.libgdx.tetris_clone.TetrisCloneMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TetrisCloneMain(), config);
+	}
+}
